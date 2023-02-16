65c33937015d5522e268343ae368cd50374c9367
Keijiro Takahashi <keijiro@gmail.com>
2023-02-16 17:41:43 +0900

    Marker UI

--- Assets/Catcher/Catcher.cs
+++ Assets/Catcher/Catcher.cs
@@ -10,18 +10,17 @@ public sealed class Catcher : MonoBehaviour
     [SerializeField] ResourceSet _resources = null;
     [SerializeField] Transform _catcher = null;
     [SerializeField] RawImage _previewUI = null;
+    [SerializeField] RectTransform _markerUI = null;
 
     BodyDetector _detector;
-    OneEuroFilter2 _filter = new OneEuroFilter2();
+
+    OneEuroFilter2 _filter =
+      new OneEuroFilter2() { Beta = 0.1f, MinCutoff = 0.5f };
+
     float2 _target;
 
     void Start()
-    {
-        _detector = new BodyDetector(_resources, 320, 240);
-
-        _filter.Beta = 0.1f;
-        _filter.MinCutoff = 0.5f;
-    }
+      => _detector = new BodyDetector(_resources, 320, 240);
 
     void OnDestroy()
     {
@@ -35,8 +34,11 @@ public sealed class Catcher : MonoBehaviour
 
         var hand = _detector.Keypoints[(int)Body.KeypointID.RightWrist];
 
-        if (hand.Score > 0.8f) _target = hand.Position;
+        var rectSize = _previewUI.rectTransform.rect.size;
+        _markerUI.anchoredPosition = hand.Position * rectSize;
+        _markerUI.GetComponent<Image>().color = new Color(1, 0, 0, hand.Score);
 
+        if (hand.Score > 0.8f) _target = hand.Position;
         var filtered = _filter.Step(Time.time, _target);
         filtered = (filtered - 0.5f) * math.float2(-2, 0.8f);
         _catcher.localPosition = math.float3(filtered, 0);
