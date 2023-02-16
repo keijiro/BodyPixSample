511597c8568bc97c1bad247df0755c12fd9e0909
Keijiro Takahashi <keijiro@gmail.com>
2023-02-16 11:22:37 +0900

    initial commit

---
+++ Assets/Tester.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Klak.TestTools;
+using BodyPix;
+
+public sealed class Tester : MonoBehaviour
+{
+    [SerializeField] ImageSource _source = null;
+    [SerializeField] Shader _previewShader = null;
+    [SerializeField] Texture2D _background = null;
+    [SerializeField] RawImage _previewUI = null;
+    [SerializeField] ResourceSet _resources = null;
+
+    BodyDetector _detector;
+    Material _material;
+
+    void Start()
+    {
+        _detector = new BodyDetector(_resources, 320, 240);
+        _material = new Material(_previewShader);
+        _previewUI.material = _material;
+    }
+
+    void OnDestroy()
+    {
+        _detector?.Dispose();
+        _detector = null;
+
+        Destroy(_material);
+        _material = null;
+    }
+
+    void Update()
+    {
+        _detector.ProcessImage(_source.Texture);
+
+        _material.SetTexture("_BgTexture", _background);
+        _material.SetTexture("_CameraTexture", _source.Texture);
+        _material.SetTexture("_MaskTexture", _detector.MaskTexture);
+    }
+}
